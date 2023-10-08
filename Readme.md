f0dd6e73f9f69088864df197fc55239c51137deb
Rohan Bomle <rohanbomle@gmail.com>
2023-10-08 22:05:24 +0100

    React web app created

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# React Test App
+
+react-node application to demonstrate how to build end to end web app.
+
+## api: Provides API to connect with databse(MongoDB).
+## Client: User inerface to interact with backend
+
+Dependencies/Framework/Concept used:
+
+1. Express - web application framework build on top of node.js
+2. React Router Dom - used for navigation in react application
+3. MongoDB - database 
+4. MUI - library to build rich/beautiful user interface
+5. React Hooks - simple JavaScript functions that we can use to isolate the reusable part from a functional component. 
+
---
+++ api/server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+var mongoClient = require("mongodb").MongoClient
+var cors = require("cors")
+const multer = require("multer")
+const app = express()
+app.use(cors())
+
+var CONNECTION_STRING = "mongodb+srv://robo:ugqRPrjJ9nX7N52@clusternodetest.kzecatu.mongodb.net/?retryWrites=true&w=majority";
+var DATABASE_NAME = "nodetest"
+var database
+
+app.listen(3001, ()=> {
+  mongoClient.connect(CONNECTION_STRING,(error,client)=> {
+    database = client.db(DATABASE_NAME)
+    console.log("Connection to MongoDB is successful")
+  });
+})
+
+app.get('/',(req, res)=>{
+  res.send("API is running on this port")
+})
+
+app.get('/api/users',(req, res)=>{
+  database.collection("userdata").find({}).toArray((error, result)=>{
+    res.send(result)
+  });
+})
+
+app.post('/api/adduser', multer().none(),(req, res)=>{
+  database.collection("userdata").count({}, function(error, numOfDocs){
+    database.collection("userdata").insertOne({
+      name:req.body.name,
+      age:req.body.age,
+      sex:req.body.sex,
+      country:req.body.country
+    });
+    res.json("Success")
+  })
+})
